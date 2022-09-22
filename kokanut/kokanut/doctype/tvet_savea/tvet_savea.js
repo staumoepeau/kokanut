3b124bda0eb0e7fa35c6540396cab604a11864ec
root <root@localhost.localdomain>
2022-09-22 01:47:31 -0800

    Fix Error Query E

--- kokanut/kokanut/doctype/tvet_savea/tvet_savea.js
+++ kokanut/kokanut/doctype/tvet_savea/tvet_savea.js
@@ -30,19 +30,12 @@ frappe.ui.form.on('TVET Savea', {
 
 		display_e6(frm);
 
-		const E2List_2 = ["Student full time","Student part-time",
-			"Home duties","Retired/ too old","None: no work, no gardening"]
-
-		if (E2List_2.includes(frm.doc.e_2)){
-			frm.set_value("end_of_survey", "THANK YOU THAT THE END OF THE SURVEY. PLEASE COMPLETE THE FEEDBACK BEFORE YOU LEAVE")
-		} else
-		{
-			frm.set_value("end_of_survey", " ")
-		}
-		
 	}
 });
 var display_e6 = function(frm){
+
+	const E2List_2 = ["Student full time","Student part-time",
+			"Home duties","Retired/ too old","None: no work, no gardening"]
 
 	const E2List_1 = ["Employer","Producing goods or services for sale/ Running a business",
 			"Employee working for wages in the public sector","Employee working for wages for a private sector",
@@ -57,5 +50,16 @@ var display_e6 = function(frm){
 		}
 	} else {
 		frm.set_df_property("e6", "hidden", 1);
+	}
+
+	if (frm.doc.e_2){
+		if (E2List_2.includes(frm.doc.e_2)){
+			frm.set_value("end_of_survey", "THANK YOU THAT THE END OF THE SURVEY. PLEASE COMPLETE THE FEEDBACK BEFORE YOU LEAVE")
+		} else
+		{
+			frm.set_value("end_of_survey", " ")
+		}
+	} else {
+		frm.set_value("end_of_survey", " ")
 	}
 }
