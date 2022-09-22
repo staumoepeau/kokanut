45426c238858ab4d4bf507d4edd0e3786ee383da
root <root@localhost.localdomain>
2022-09-22 01:05:46 -0800

    Fix Display Error

--- kokanut/kokanut/doctype/tvet_savea/tvet_savea.js
+++ kokanut/kokanut/doctype/tvet_savea/tvet_savea.js
@@ -22,6 +22,7 @@ frappe.ui.form.on('TVET Savea', {
 	},
 
 	onload: function(frm){
+
 		display_e6(frm);
 	},
 
@@ -33,11 +34,11 @@ frappe.ui.form.on('TVET Savea', {
 			"Home duties","Retired/ too old","None: no work, no gardening"]
 
 		if (E2List_2.includes(frm.doc.e_2)){
-			frm.set_value("end_of_survey", "THANK YOU THAT THE END OF THE SURVEY")
+			frm.set_value("end_of_survey", "THANK YOU THAT THE END OF THE SURVEY. PLEASE COMPLETE THE FEEDBACK BEFORE YOU LEAVE")
+		} else
+		{
+			frm.set_value("end_of_survey", " ")
 		}
-
-		
-
 		
 	}
 });
@@ -47,16 +48,14 @@ var display_e6 = function(frm){
 			"Employee working for wages in the public sector","Employee working for wages for a private sector",
 			"Producing goods or services for own/family consumption","Unpaid family worker (family business, plantation)",
 			"Unpaid family worker (helps with essential household duties)", "Voluntary work/community work (for free)"]
-		
+	
+	if (frm.doc.e_2){
 		if (E2List_1.includes(frm.doc.e_2)){
-
 			frm.set_df_property("e6", "hidden", 0);
-	//	alert("Not Hidden")
-		
 		} else {
-
 			frm.set_df_property("e6", "hidden", 1);
-	//	alert("Hidden")
-
 		}
+	} else {
+		frm.set_df_property("e6", "hidden", 1);
+	}
 }
